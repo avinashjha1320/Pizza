d6a448ec4fcffff37bf721e4f925baaeb11879c7
avinashjha1320 <jhaavinash0118@gmail.com>
2020-12-31 19:09:20 +0530

    updated to 2021

---
+++ Code/app/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    role: { type: String, default: 'customer' },
+    password: { type: String, required: true }
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', userSchema)
---
+++ Code/routes/web.js
@@ -0,0 +1,36 @@
+const authController = require('../app/http/controllers/authController')
+const cartController = require('../app/http/controllers/customers/cartController')
+const orderController = require('../app/http/controllers/customers/orderController')
+const homeController = require('../app/http/controllers/homeController')
+const guest = require('../app/http/middleware/guest')
+const auth = require('../app/http/middleware/auth')
+const admin = require('../app/http/middleware/admin')
+const adminOrderController = require('../app/http/controllers/admin/orderController')
+const statusController = require('../app/http/controllers/admin/statusController')
+
+function initRoutes(app) {
+    app.get('/', homeController().index)
+
+    app.get('/login', guest, authController().login)
+    app.post('/login', authController().postLogin)
+
+    app.get('/register', guest, authController().register)
+    app.post('/register', authController().postregister)
+
+    app.post('/logout', authController().logout)
+
+    app.get('/cart', cartController().index)
+    app.post('/update-cart', cartController().update)
+
+    app.post('/orders', auth, orderController().store)
+    app.get('/customer/orders', auth, orderController().index)
+    app.get('/customer/orders/:id', auth, orderController().show)
+
+    app.get('/admin/orders', auth, adminOrderController().index)
+    app.post('/admin/order/status', admin, statusController().update)
+}
+module.exports = initRoutes
+
+//(req, res) => {
+//    res.render('home')
+//}
